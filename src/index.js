f5704333b04d8080a6c80ef7e223f8739fa5f51b
Alexey Ostapenko <alexeyostapenko@MacBook-Air-Alexey.local>
2018-02-27 13:11:09 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let solution = [];
+  let parts = equation.split('*');
+  let a = parts[0];
+  if (a[0] === '-')
+  a = -parseInt(a.substring(1))
+  else a = parseInt(a);
+
+  let b = parts[1].substring(5);
+  if (b[0] === '-')
+  b = -parseInt(b.substring(2));
+  if (b[0] === '+')
+  b = parseInt(b.substring(2))	
+  
+  let c = parts[2].substring(3);
+  if (c[0] === '-')
+  c = -parseInt(c.substring(2));
+  if (c[0] === '+')
+  c = parseInt(c.substring(2))
+
+  let D = parseInt(b * b - 4 * a * c);
+  let xF = (-b + Math.sqrt(D)) / 2 / a;
+  let xS = (-b - Math.sqrt(D)) / 2 / a;
+
+  if (xF < xS)
+  return solution = [xF.toFixed(), xS.toFixed()];
+  if (xF > xS)
+  return solution = [xS.toFixed(), xF.toFixed()];
+
+
 }
